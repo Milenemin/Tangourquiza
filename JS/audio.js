bd5455bd3b04675b3f1cbe606e1e991f314bf2a8
milene <mimi0003@stud.kea.dk>
2026-02-16 13:44:50 +0100

    done

--- JS/audio.js
+++ JS/audio.js
@@ -1,10 +1,26 @@
-const button = document.getElementById("audioBtn");
-const audio = document.getElementById("audio");
+// const button = document.getElementById("audioBtn");
+// const audio = document.getElementById("audio");
 
-button.addEventListener("click", () => {
-  if (audio.paused) {
+// button.addEventListener("click", () => {
+//   if (audio.paused) {
+//     audio.play();
+//   } else {
+//     audio.pause();
+//   }
+// });
+const playButton = document.getElementById("playButton");
+const audio = document.getElementById("tangoAudio");
+
+let isPlaying = false;
+
+playButton.addEventListener("click", () => {
+  if (!isPlaying) {
     audio.play();
+    isPlaying = true;
+    playButton.classList.add("playing");
   } else {
     audio.pause();
+    isPlaying = false;
+    playButton.classList.remove("playing");
   }
 });
